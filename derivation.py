6f5bed6824a743c62846f8f679cf95330886549a
admin <nnamdichibuzor777@gmail.com>
2025-09-27 02:11:55 -0700

    update derivation

--- derivation.py
+++ derivation.py
@@ -1,21 +1,4 @@
-# from sympy import symbols, Matrix, solve, zeros, pprint, sin, cos, diff, expand
-# xc, xc_dot, yc, yc_dot, mc, xp, xp_dot, yp, yp_dot, mp, theta, theta_dot, l = symbols("xc ẋc yc ẏc mc xp ẋp yp ẏp mp θ θ̇ l")
-
-# xp = xc - (l/2)*sin(theta)
-# xp_dot = xc_dot - (l/2)*cos(theta)*theta_dot
-# yp = (l/2)*cos(theta)
-# yp_dot = (l/2)*sin(theta)*theta_dot
-
-
-# Tcart = (1/2)*mc*xc_dot**2
-# Tpole = (1/2)*mp*(xp_dot**2 + yp_dot**2)
-# T = Tcart + Tpole
-
-# pprint(T)
-
-
-
-from sympy import symbols, Function, diff, sin, cos, Derivative, pprint, Eq, solve
+from sympy import symbols, Function, diff, sin, cos, Derivative, pprint, Eq, solve, Matrix, zeros
 
 
 def pprint_(expr):
@@ -25,7 +8,7 @@ def pprint_v(expr):
     pprint_(expr.expand())
 
 # constants
-mc, mp, l, Icm = symbols('mc mp l Icm')
+mc, mp, l, Icm, g= symbols('mc mp l Icm g')
 
 # variables
 t, u = symbols('t u')
@@ -37,7 +20,6 @@ xc_dot_sym, xc_ddot_sym, xp_dot_sym, yp_dot_sym, theta_dot_sym, theta_ddot_sym =
 
 theta = Function('θ')(t)
 xc = Function('xc')(t)
-yc = Function('yc')(t)
 xp = xc - (l/2)*sin(theta)
 yp = (l/2)*cos(theta)
 
@@ -61,7 +43,7 @@ Tpole = (1/2)*mp*(xp_dot**2 + yp_dot**2) + (1/2)*Icm*theta_dot**2
 T = Tcart + Tpole
 
 Vcart = 0
-Vpole = mp*(l/2)*cos(theta)
+Vpole = mp*g*(l/2)*cos(theta)
 V = Vcart + Vpole
 
 
@@ -80,9 +62,57 @@ ddt_partialL_xc_dot_sym = diff(partialL_xc_dot_sym, t)
 eom_theta = Eq(ddt_partialL_theta_dot_sym - partialL_theta_sym, 0)
 eom_xc = Eq(ddt_partialL_xc_dot_sym - partialL_xc_sym, u)
 
-sol = solve([eom_theta, eom_xc], [Derivative(xc, t, 2), Derivative(theta, t, 2)])
-print(sol)
+# Isolate ẍc, θ̈ 
+sol = solve( (eom_theta, eom_xc), (Derivative(xc, t, 2), Derivative(theta, t, 2)) )
+xc_ddot = sol[Derivative(xc, (t, 2))]
+theta_ddot = sol[Derivative(theta, (t, 2))]
+
+# Define state variables: x1 = θ, x2 = θ̇, x3 = xc, x4 = ẋc
+x1, x2, x3, x4 = symbols('x1 x2 x3 x4')
+
+to_state = {
+    theta: x1,
+    theta_dot: x2,
+    xc: x3,
+    xc_dot: x4
+}
+
+xt = Matrix([
+    theta, 
+    theta_dot, 
+    xc, 
+    xc_dot
+]).subs(to_state)
+
+xt_dot = Matrix([
+    theta_dot, 
+    theta_ddot, 
+    xc_dot,
+    xc_ddot
+]).subs(to_state)
+
+ut = Matrix([u])
 
 
 
-# define x = 
+
+sol_eq = solve( 
+    xt_dot.subs([(u, 0)]) - zeros(4, 1), 
+    (x1, x2, x3, x4) 
+)
+
+
+to_up = zip((x1, x2, x3, x4), sol_eq[0])
+Aup = xt_dot.jacobian(xt).subs(to_up)
+Bup = xt_dot.jacobian(xt).subs(to_up)
+pprint(Aup)
+
+to_down = zip((x1, x2, x3, x4), sol_eq[1])
+Adown = xt_dot.jacobian(xt).subs(to_down)
+Bdown = xt_dot.jacobian(xt).subs(to_down)
+pprint(Adown)
+
+
+
+
+
